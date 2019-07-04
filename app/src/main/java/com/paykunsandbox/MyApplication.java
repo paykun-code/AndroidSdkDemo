28e17c87ef70bf7a8dba88f3038161c5c2793fea
Bhavikom <bhavikmakvanarock@gmail.com>
2019-07-04 16:43:03 +0530

    => commited on 4th july

--- app/src/main/java/com/paykunsandbox/MyApplication.java
+++ app/src/main/java/com/paykunsandbox/MyApplication.java
@@ -1,10 +1,13 @@
 package com.paykunsandbox;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
     }
 }
